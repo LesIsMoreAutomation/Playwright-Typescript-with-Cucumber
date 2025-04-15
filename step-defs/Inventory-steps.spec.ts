747e25c68d8c4beae2e7020226518e6cc10629ad
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2025-04-15 14:07:48 +0200

    Update Inventory-steps.spec.ts

--- step-defs/Inventory-steps.spec.ts
+++ step-defs/Inventory-steps.spec.ts
@@ -1,24 +1,18 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { getPage } from "../config/global-setup";
-import { InventoryPage } from "../page-objects/products-Page/InventoryPage";
+import { page, BasePage } from "../config/global-setup";
+
 Then("I click on the {string}", async function (text) {
-    const page = getPage(this);
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.clickByText(text);
-    await page.screenshot({ path: 'test-report/screenshot.png' });
+    await BasePage.InventoryPage.clickByText(text);
+    await page.screenshot({ path: 'test-report/screenshot.png' })
 });
-Then("the user should see {int} items in the cart", async function (itemCount) {
-    const page = getPage(this);
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.verifyCartItemCount(itemCount);
+
+Then("the user should see {int} items in the cart", async function (text) {
+    await BasePage.InventoryPage.verifyCartItemCount(text);
 });
 When("the user filters items by pricing", async function () {
-    const page = getPage(this);
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.filterItemsByPricing();
+    await BasePage.InventoryPage.filterItemsByPricing()
 });
 Then("the user should see the first item as the lowest and the last item as the highest in price", async function () {
-    const page = getPage(this);
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.verifyPriceOrder();
+    await BasePage.InventoryPage.verifyPriceOrder();
+
 });
