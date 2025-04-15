7b52487dbba21566d31e2d855ee595b547c6908d
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2025-04-15 09:53:39 +0200

    Update Inventory-steps.spec.ts

--- step-defs/Inventory-steps.spec.ts
+++ step-defs/Inventory-steps.spec.ts
@@ -1,18 +1,18 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { page, baseBage } from "../config/global-setup";
+import { page, getPage } from "../config/global-setup";
 
 Then("I click on the {string}", async function (text) {
-    await baseBage.InventoryPage.clickByText(text);
+    await getPage.InventoryPage.clickByText(text);
     await page.screenshot({ path: 'test-report/screenshot.png' })
 });
 
 Then("the user should see {int} items in the cart", async function (text) {
-    await baseBage.InventoryPage.verifyCartItemCount(text);
+    await getPage.InventoryPage.verifyCartItemCount(text);
 });
 When("the user filters items by pricing", async function () {
-    await baseBage.InventoryPage.filterItemsByPricing()
+    await getPage.InventoryPage.filterItemsByPricing()
 });
 Then("the user should see the first item as the lowest and the last item as the highest in price", async function () {
-    await baseBage.InventoryPage.verifyPriceOrder();
+    await getPage.InventoryPage.verifyPriceOrder();
 
 });
