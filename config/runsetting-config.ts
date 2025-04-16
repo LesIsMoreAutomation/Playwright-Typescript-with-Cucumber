62c5e80af47007aceb8cb0deb8b7bc8d6323e40d
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2025-04-16 11:29:16 +0200

    Update runsetting-config.ts

--- config/runsetting-config.ts
+++ config/runsetting-config.ts
@@ -3,10 +3,11 @@ import { LaunchOptions } from "@playwright/test";
 const browserOptions: LaunchOptions = {
   tracesDir: "traces",
   headless: true,
+  channel: 'msedge',
 };
 
 export const config = {
-  browser: process.env.BROWSER || "chromium",
+  //browser: process.env.BROWSER || "chromium",
   browserOptions,
   //baseUrl: "https://www.saucedemo.com/",
 };
