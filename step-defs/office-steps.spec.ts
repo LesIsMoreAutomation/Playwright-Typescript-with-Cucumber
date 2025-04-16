346a00fa6e731608aa8ef39bda180a12783b74e8
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2025-04-16 10:47:06 +0200

    Update office-steps.spec.ts

--- step-defs/office-steps.spec.ts
+++ step-defs/office-steps.spec.ts
@@ -1,20 +1,20 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { page, baseBage } from "../config/global-setup";
+import { page, basePage } from "../config/global-setup";
 import {BasePage} from "../page-objects/login-signin/BasePage";
 
 
 Given("I enter my username {string}", async function (username) {
-    await baseBage.OfficePage.enterUsername(username);
+    await basePage.OfficePage.enterUsername(username);
 });
 
 Given("I enter my password {string}", async function (password) {
-    await baseBage.OfficePage.enterPassword(password);
+    await basePage.OfficePage.enterPassword(password);
 });
 
 Then("I click on a {string}", async function (text) {
-    await baseBage.OfficePage.clickOnText(text);
+    await basePage.OfficePage.clickOnText(text);
 });
 
 When("I search for {string}", async function (text) {
-    await baseBage.SportPage.enterPlaceholderInput(text)
+    await basePage.SportPage.enterPlaceholderInput(text)
 });
