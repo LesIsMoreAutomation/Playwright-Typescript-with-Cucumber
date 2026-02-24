2db5a1ea88f4b35860a3c6ed3dd2f09e72037522
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2026-02-24 13:15:17 +0200

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-// Path to your step definitions
-const stepsPath = "step-defs/*.ts"; 
-
 // Get the feature folder or file path from command-line arguments
 const featureArg = process.argv.find(arg => arg.includes('features/'));
 let featurePath;
@@ -12,29 +9,28 @@ if (featureArg) {
   const fullPath = path.resolve(featureArg);
   if (fs.existsSync(fullPath)) {
     if (fs.lstatSync(fullPath).isDirectory()) {
-      featurePath = path.join(fullPath, '**/*.feature'); 
+      featurePath = path.join(fullPath, '**/*.feature'); // Run all .feature files in the folder
     } else if (fs.lstatSync(fullPath).isFile() && fullPath.endsWith('.feature')) {
-      featurePath = fullPath; 
+      featurePath = fullPath; // Run the specific .feature file
     } else {
-      throw new Error("Invalid path.");
+      throw new Error("Invalid path. Provide a folder containing .feature files or a specific .feature file.");
     }
   } else {
     throw new Error(`Path "${featureArg}" does not exist.`);
   }
 } else {
-  featurePath = "features/**/*.feature"; 
+  featurePath = "features/**/**/*.feature"; // Default to all .feature files
 }
 
+// Extract the feature file name for reporting
 const featureFileName = featureArg ? featureArg.split('/').pop().replace('.feature', '') : 'all-features';
 
 const options = [
-  // Use --loader instead of --require-module for ESM/Typescript compatibility
-  "--loader ts-node/esm", 
-  `--import ${stepsPath}`, 
+  "--require-module ts-node/register",
+  "--require src/step-defs/*.spec.ts",
   `-f json:test-report/${featureFileName}/${featureFileName}_cucumber_report.json`,
   `-f junit:test-report/${featureFileName}/${featureFileName}_cucumber_report.xml`,
-  "--format summary",
-  "--retry 1"
+  "--retry", "1"
 ].join(" ");
 
 let runsettings = [featurePath, options].join(" ");
