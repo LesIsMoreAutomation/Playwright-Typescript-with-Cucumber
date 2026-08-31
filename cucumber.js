51d61f897fd0b5cc688c1e5c174af893d5b33f6a
Lesibana Moselakgomo <128179698+LesIsMoreAutomation@users.noreply.github.com>
2026-08-31 07:55:33 +0200

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -27,7 +27,7 @@ const featureFileName = featureArg ? featureArg.split('/').pop().replace('.featu
 
 const options = [
   "--require-module ts-node/register",
-  "--require src/**/*.ts",
+  "--require **/*.ts",
   `-f json:test-report/${featureFileName}/${featureFileName}_cucumber_report.json`,
   `-f junit:test-report/${featureFileName}/${featureFileName}_cucumber_report.xml`,
   "--retry", "1"
